487dbc0d6c7d04c37643224d615c7b6fe681c67e
njtan142 <njtan142@penguin>
2020-06-08 06:08:31 +0800

    better

--- script/script.js
+++ script/script.js
@@ -26,19 +26,19 @@ var img2 = document.getElementById("profile-photo2");
 var img3 = document.getElementById("profile-photo3");
 var img4 = document.getElementById("profile-photo4");
 var img5 = document.getElementById("profile-photo5");
-if(img1 != null){
+if (img1 != null) {
   image(img1);
 }
-if(img2 != null){
+if (img2 != null) {
   image(img2);
 }
-if(img3 != null){
+if (img3 != null) {
   image(img3);
 }
-if(img4 != null){
+if (img4 != null) {
   image(img4);
 }
-if(img5 != null){
+if (img5 != null) {
   image(img5);
 }
 let b = 0;
@@ -51,43 +51,48 @@ function image(img) {
     var captionText = document.getElementById("caption");
     var x = window.matchMedia("(max-width: 400px)")
 
-    function media(){
-    modal.style.display = "flex";
-    modal.style.height = "100vh"
-    modal.style.overflow = "scroll"
-    modal.style.flexDirection = "column";
-    modal.style.alignItems = "center";
-    }
-    if(x.matches){
-      media();
-    }else{
+    function media() {
       modal.style.display = "flex";
-    modal.style.height = "100vh"
-    modal.style.alignItems = "center";
+      modal.style.height = "100vh"
+      modal.style.overflow = "scroll"
+      modal.style.flexDirection = "column";
+      modal.style.alignItems = "center";
+    }
+    if (x.matches) {
+      media();
+    } else {
+      modal.style.display = "flex";
+      modal.style.height = "100vh"
+      modal.style.alignItems = "center";
       modal.style.flexDirection = "row";
       modal.style.justifyContent = "center"
     }
+    modalImg.style.display = "block";
     modalImg.src = img.src;
     captionText.innerHTML = img.alt;
     var span = document.getElementsByClassName("close")[0];
-b++
+
     // When the user clicks on <span> (x), close the modal
     span.onclick = function () {
       modal.style.display = "none";
     }
     var closeImg = document.getElementsByClassName("modal-content")[0];
     var closeImg1 = document.getElementById("caption");
-    var closeImg2 = document.getElementsByClassName("modal")[0];
-      closeImg.onclick = function () {
-        modal.style.display = "none";
-  }
-  closeImg1.onclick = function () {
-    modal.style.display = "none";
-}
-closeImg2.onclick = function () {
-  modal.style.display = "none";
-}
-}
-  // Get the <span> element that closes the modal
-}
+    
+    closeImg.onclick = function () {
+      modal.style.display = "none";
+    }
+    closeImg1.onclick = function () {
+      b++
+      if (b % 2 == 1) {
+        
+        modalImg.style.display = "none";
+      }else{
+        modalImg.style.display = "block";
+        
+      }
 
+    }
+    // Get the <span> element that closes the modal
+  }
+}
